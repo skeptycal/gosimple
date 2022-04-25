7d28f3862274ebda3e8edcfa9d42aa7dbdeda419
Michael Treanor <skeptycal@gmail.com>
2022-04-25 17:13:31 -0500

    api updates

---
+++ cmd/examples/delsubs/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/fs"
+	"os"
+
+	"github.com/skeptycal/gosimple/cli/errorlogger"
+	"github.com/skeptycal/gosimple/os/gofile"
+)
+
+var (
+	log       = errorlogger.New()
+	forceFlag bool
+)
+
+func FindDirs(dir, pattern string) ([]fs.DirEntry, error) {
+
+}
+
+func init() {
+	flag.BoolVar(&forceFlag, "force", false, "force deletion of located files")
+
+}
+
+func main() {
+
+	// find . -type f -wholename "./*/go.sum" -exec rm -rf {} +
+
+	// FindDirs locates directories from the root 'dir' that
+	// match the pattern.
+
+	dir := gofile.PWD()
+
+	patterns := []string{"go.mod", "go.sum", "go.sum", ".git"}
+
+	for _, pattern := range patterns {
+		list, err := FindDirs(dir, pattern)
+		if err != nil {
+			log.Errorf("error finding directories: %v", err)
+		}
+
+		for _, file := range list {
+			fmt.Printf("%8v %s\n", file.Type().Perm(), file.Name())
+			if forceFlag {
+				os.Remove(file.Name())
+			}
+		}
+	}
+}
