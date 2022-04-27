a3b212778561a8b35d0e836676157cf7025a3759
Michael Treanor <skeptycal@gmail.com>
2022-04-26 22:54:30 -0500

    api updates

--- os/shpath/shpath_test.go
+++ os/shpath/shpath_test.go
@@ -1,24 +1,18 @@
 package shpath
 
 import (
+	"strings"
 	"testing"
 )
 
 var fakeString = ""
 
-var stringFuncs = []struct {
-	name string
-	fn   func(string) string
-}{
-	{"(cache loading ... ignore this one)", NormalizeNL},
-	{"for loop", normalizeNLForLoop},
-	{"bytes.Replace", normalizeNewlinesBytesWrapper},
-	{"strings.Replace", normalizeNewlinesString},
-	{"aliased", NormalizeNL},
-	{"wrapped", normalizeWrapped},
-	// {"global strings.Builder", normalizeGlobalStringsBuilder},
-	{"use strings.Builder", normalizeStringsBuilder},
-}
+var (
+	C8  = strings.Repeat("Repeat", 1<<8)
+	C16 = strings.Repeat("Repeat", 1<<16)
+	C24 = strings.Repeat("Repeat", 1<<24)
+	// C48 = strings.Repeat("Repeat", 1<<48) // overflows string cap
+)
 
 var stringModifyFuncs = []struct {
 	name string
@@ -27,19 +21,101 @@ var stringModifyFuncs = []struct {
 	{"DropDupes", DropDupes},
 }
 
-// BenchmarkNormalize/for_loop-8         	 2511970	       529.6 ns/op	     336 B/op	       3 allocs/op
-// BenchmarkNormalize/bytes.Replace-8    	 2226400	       532.8 ns/op	     448 B/op	       4 allocs/op
-// BenchmarkNormalize/strings.Replace-8  	 3018738	       497.9 ns/op	     224 B/op	       2 allocs/op
-func BenchmarkNormalize(b *testing.B) {
-	arg := "asdlfkn2;leja-9cv8yh	-2piouej4b-	2u9hnasdj;lasdjflkasnvj8q92nn2den\rasdfklw\r\nl;jkqw;cijhpoiqjwd\n\njl-9c8vn-	wd"
+/* Benchmark results
+/(cache_loading_..._ignore_this_one)-8         	 5062321	       310.8 ns/op	     224 B/op	       2 allocs/op
+/using_bytes_package-8                         	 3359154	       374.0 ns/op	     224 B/op	       2 allocs/op
+/for_loop-8                                    	 3393852	       328.8 ns/op	     336 B/op	       3 allocs/op
+/bytes.Replace-8                               	 3850560	       315.6 ns/op	     448 B/op	       4 allocs/op
+/strings.Replace-8                             	 4760284	       252.3 ns/op	     224 B/op	       2 allocs/op
+/aliased-8                                     	 4771462	       253.4 ns/op	     224 B/op	       2 allocs/op
+/wrapped-8                                     	 4706868	       255.0 ns/op	     224 B/op	       2 allocs/op
+/use_strings.Builder-8                         	 2104315	       589.7 ns/op	     224 B/op	       2 allocs/op
 
-	for _, bb := range stringFuncs {
-		b.Run(bb.name, func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				fakeString = bb.fn(arg)
-			}
-		})
+* after streamlining a bit ...
+/(cache_loading_..._ignore_this_one)-8         	 4232391	       273.3 ns/op	     224 B/op	       2 allocs/op
+/using_bytes_package_(unsafe_conversion)-8     	 3621552	       353.8 ns/op	     224 B/op	       2 allocs/op
+/using_bytes_package-8                         	 2904105	       414.3 ns/op	     448 B/op	       4 allocs/op
+/strings.Replace-8                             	 4628241	       258.3 ns/op	     224 B/op	       2 allocs/op
+/for_loop-8                                    	 2780509	       392.4 ns/op	     352 B/op	       3 allocs/op
+/use_strings.Builder_with_loop-8               	 1927165	       610.9 ns/op	     224 B/op	       2 allocs/op
+/wrapped_essential_Go_book_(wrapped)-8         	 3853898	       324.1 ns/op	     448 B/op	       4 allocs/op
+/aliased-8                                     	 4565563	       259.4 ns/op	     224 B/op	       2 allocs/op
+
+/(cache_loading_..._ignore_this_one)#01-8      	11576916	       100.8 ns/op	      16 B/op	       1 allocs/op
+/using_bytes_package_(unsafe_conversion)#01-8  	 9021549	       128.2 ns/op	      16 B/op	       1 allocs/op
+/using_bytes_package#01-8                      	 8336128	       143.4 ns/op	      32 B/op	       2 allocs/op
+/strings.Replace#01-8                          	12015027	        98.21 ns/op	      16 B/op	       1 allocs/op
+/... error stopped further benchmarks ...
+
+/(cache_loading_..._ignore_this_one)-8         	 4410556	       302.3 ns/op	     224 B/op	       2 allocs/op
+/using_bytes_package_(unsafe_conversion)-8     	 3232776	       371.9 ns/op	     224 B/op	       2 allocs/op
+/using_bytes_package-8                         	 2617440	       437.8 ns/op	     448 B/op	       4 allocs/op
+/strings.Replace-8                             	 4622386	       264.0 ns/op	     224 B/op	       2 allocs/op
+/for_loop-8                                    	 3410743	       345.5 ns/op	     336 B/op	       3 allocs/op
+/use_strings.Builder_with_loop-8               	 2069804	       590.7 ns/op	     224 B/op	       2 allocs/op
+/wrapped_essential_Go_book_(wrapped)-8         	 3831040	       315.9 ns/op	     448 B/op	       4 allocs/op
+/aliased-8                                     	 4572475	       256.6 ns/op	     224 B/op	       2 allocs/op
+
+/(cache_loading_..._ignore_this_one)#01-8      	11324400	       105.4 ns/op	      16 B/op	       1 allocs/op
+/using_bytes_package_(unsafe_conversion)#01-8  	 9021105	       145.1 ns/op	      16 B/op	       1 allocs/op
+/using_bytes_package#01-8                      	 7883144	       145.2 ns/op	      32 B/op	       2 allocs/op
+/strings.Replace#01-8                          	11732355	       102.5 ns/op	      16 B/op	       1 allocs/op
+/for_loop#01-8                                 	17830664	        67.62 ns/op	      32 B/op	       2 allocs/op
+/use_strings.Builder_with_loop#01-8            	 5625138	       214.1 ns/op	      16 B/op	       1 allocs/op
+/wrapped_essential_Go_book_(wrapped)#01-8      	 7574922	       159.5 ns/op	      48 B/op	       3 allocs/op
+/aliased#01-8                                  	11430462	       104.0 ns/op	      16 B/op	       1 allocs/op
+
+/(cache_loading_..._ignore_this_one)#02-8      	 7569651	       167.3 ns/op	      32 B/op	       2 allocs/op
+/using_bytes_package_(unsafe_conversion)#02-8  	 5101627	       220.3 ns/op	      32 B/op	       2 allocs/op
+/using_bytes_package#02-8                      	 4746549	       245.4 ns/op	      48 B/op	       3 allocs/op
+/strings.Replace#02-8                          	 6563895	       158.7 ns/op	      32 B/op	       2 allocs/op
+/for_loop#02-8                                 	16517322	        74.73 ns/op	      32 B/op	       2 allocs/op
+/use_strings.Builder_with_loop#02-8            	 2990830	       389.4 ns/op	      48 B/op	       3 allocs/op
+/wrapped_essential_Go_book_(wrapped)#02-8      	 6420805	       181.9 ns/op	      48 B/op	       3 allocs/op
+/aliased#02-8                                  	 7304424	       165.2 ns/op	      32 B/op	       2 allocs/op
+
+/(cache_loading_..._ignore_this_one)#03-8      	 2660059	       424.8 ns/op	       0 B/op	       0 allocs/op
+/using_bytes_package_(unsafe_conversion)#03-8  	 2075952	       616.7 ns/op	       0 B/op	       0 allocs/op
+/using_bytes_package#03-8                      	  698600	      1795 ns/op	   12288 B/op	       2 allocs/op
+/strings.Replace#03-8                          	 2745063	       415.8 ns/op	       0 B/op	       0 allocs/op
+/for_loop#03-8                                 	   79858	     14410 ns/op	   18432 B/op	       3 allocs/op
+/use_strings.Builder_with_loop#03-8            	 1000000	      1245 ns/op	       0 B/op	       0 allocs/op
+/wrapped_essential_Go_book_(wrapped)#03-8      	  381115	      3024 ns/op	   24576 B/op	       4 allocs/op
+/aliased#03-8                                  	 2702842	       412.9 ns/op	       0 B/op	       0 allocs/op
+*/
+
+func BenchmarkNormalize(b *testing.B) {
+	args := []string{
+		"asdlfkn2;leja-9cv8yh	-2piouej4b-	2u9hnasdj;lasdjflkasnvj8q92nn2den\rasdfklw\r\nl;jkqw;cijhpoiqjwd\n\njl-9c8vn-	wd",
+		"\r asdfsa \r",
+		"\r\nasdfa\t\n\n\r\r",
+		strings.Repeat("Repeat", 1024),
 	}
+
+	for _, arg := range args {
+		for _, bb := range stringFuncs {
+			b.Run(bb.name, func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					fakeString = bb.fn(arg)
+				}
+			})
+		}
+	}
+}
+
+var stringFuncs = []struct {
+	name string
+	fn   func(string) string
+}{
+	{"(cache loading ... ignore this one)", NormalizeNL},
+	{"normalize (unsafe conversion)", normalize},
+	{"normalizeBytesTester", normalizeBytesTester},
+	{"normalizeNewlinesString", normalizeNewlinesString},
+	{"normalizeNLForLoop", normalizeNLForLoop},
+	{"normalizeLoop", normalizeLoop},
+	// {"use strings.Builder", normalizeStringsBuilder},
+	{"normalizeNewlinesBytesWrapper", normalizeNewlinesBytesWrapper},
+	// {"aliased", NormalizeNL},
 }
 
 func TestNormalize(t *testing.T) {
@@ -51,7 +127,6 @@ func TestNormalize(t *testing.T) {
 		args args
 		want string
 	}{
-		// TODO: Add test cases.
 		{"1234567890", args{"1234567890"}, "1234567890"},
 		{"\n1234567890", args{"\n1234567890"}, "\n1234567890"},
 		{"\r1234567890", args{"\r1234567890"}, "\n1234567890"},
@@ -62,9 +137,13 @@ func TestNormalize(t *testing.T) {
 		{"fake", args{"fake"}, "fake"},
 		{"fa\rke", args{"fa\rke"}, "fa\nke"},
 		{"fa\r\nke", args{"fa\r\nke"}, "fa\nke"},
+		{"Repeat x 1<<8", args{C8}, C8},
+		{"Repeat x 1<<8", args{C16}, C16},
+		{"Repeat x 1<<8", args{C24}, C24},
+		// {"Repeat x 1<<8", args{C48}, C48},
 	}
-	for _, tt := range tests {
-		for _, ff := range stringFuncs {
+	for _, ff := range stringFuncs {
+		for _, tt := range tests {
 			name := ff.name + "(" + tt.name + ")"
 			t.Run(name, func(t *testing.T) {
 				if got := ff.fn(tt.args.s); got != tt.want {
--- os/shpath/stringutil.go
+++ os/shpath/stringutil.go
@@ -3,6 +3,8 @@ package shpath
 import (
 	"bytes"
 	"strings"
+
+	"github.com/skeptycal/gosimple/types/convert"
 )
 
 // NormalizeNL normalizes newlines to the popular
@@ -16,97 +18,142 @@ import (
 // one is aliased to this function.
 var NormalizeNL = normalizeNewlinesString
 
+// DropDupes replaces consecutive duplicates of only specific 'sep' strings with a single 'sep'
+func DropDupes(s string, substr string) string {
+	return dropDupe(s, substr)
+}
+
+const (
+	rConst  = "\r"   // single carriage return (older Mac newline)
+	rnConst = "\r\n" // older Windows newline
+	rrConst = "\r\r" // duplicate carriage return (no reason for existing)
+	nnConst = "\n\n" // duplicate newlines (may be intended)
+	newline = "\n"   // normalized newline
+)
+
+var oldList = []string{"\r", "\r\n", "\n\n"}
+
+// byte slices of convertion string constants
+var (
+	r  = []byte(rConst)
+	rr = []byte(rConst + rConst)
+	rn = []byte(rnConst)
+	nn = []byte(newline + newline)
+	nl = []byte(newline)
+)
+
+// conversion function shortcut aliases
+var (
+	b2s = convert.UnsafeBytesToString
+	s2b = convert.UnsafeStringToBytes
+)
+
+// normalize is string version of the simplest, and likely
+// quickest, line ending normalization function.
+//
+// Benchmarks in comments.
+func normalize(in string) string {
+	return b2s(normalizeBytes(s2b(in)))
+}
+
+func normalizeBytesTester(s string) string {
+	b := []byte(s)
+	b = normalizeBytes(b)
+	return string(b)
+}
+
+// normalizeBytes is the byte slice version of the
+// simplest, and likely quickest, line ending
+// normalization function.
+//
+// Benchmarks in comments.
+func normalizeBytes(b []byte) []byte {
+	// remove \r\n
+	for bytes.Contains(b, rn) {
+		b = bytes.ReplaceAll(b, rn, nl)
+	}
+
+	// dedupe any remaining duplicate r's
+	for bytes.Contains(b, rr) {
+		b = bytes.ReplaceAll(b, r, nl)
+	}
+
+	// replace any remaining \r with \n
+	for bytes.Contains(b, r) {
+		b = bytes.ReplaceAll(b, r, nl)
+	}
+
+	return b
+}
+
+// normalizeNewlinesString uses strings.Replace
+//
+// for benchmarking comparisons
+func normalizeNewlinesString(d string) string {
+	// replace CR LF \r\n (windows) with LF \n (unix)
+	d = strings.ReplaceAll(d, rnConst, newline)
+	// replace CF \r (mac) with LF \n (unix)
+	d = strings.ReplaceAll(d, rConst, newline)
+	return d
+}
+
 func normalizeWrapped(s string) string {
 	return normalizeNewlinesString(s)
 }
 
-// DropDupes replaces consecutive duplicates of only specific 'sep' strings with a single 'sep'
-func DropDupes(s string, sep string) string {
-	return strings.Replace(s, sep+sep, sep, -1)
+// for benchmarking comparisons
+func normalizeNLForLoop(s string) string {
+
+	old := []byte(s)
+	new := make([]byte, 0, len(s))
+
+	for i, c := range old {
+		switch c {
+		case '\r':
+			if i >= len(old) || old[i+1] != '\n' {
+				new = append(new, '\n')
+			}
+		case '\n':
+			new = append(new, '\n')
+		default:
+			new = append(new, c)
+		}
+	}
+	return string(new)
 }
 
-/* ----------------------------------------------------------------
-BenchmarkNormalize/for_loop-8                   2278214	       522.9 ns/op	     336 B/op	       3 allocs/op
-BenchmarkNormalize/bytes.Replace-8              2078160	       574.4 ns/op	     448 B/op	       4 allocs/op
-BenchmarkNormalize/strings.Replace-8            4335457	       276.2 ns/op	     224 B/op	       2 allocs/op
-BenchmarkNormalize/wrapped-8                    4339246	       279.2 ns/op	     224 B/op	       2 allocs/op
-PASS
-*/
+func dropDupe(s, sub string) string {
+	if strings.Contains(s, sub) {
+		return strings.Replace(s, sub+sub, sub, -1)
+	}
+	return s
+}
+
+func normalizeLoop(s string) string {
+	for _, olds := range oldList {
+		s = dropDupe(s, olds)
+		s = strings.Replace(s, olds, newline, -1)
+	}
+	return s
+}
 
 func normalizeStringsBuilder(s string) string {
-	const old = "\r"
-	const old2 = "\r\n"
-	const new = "\n"
-	var n = -1
-
-	// if old == s || old2 == s {
-	// 	return s // avoid allocation
-	// }
-
-	// Compute number of replacements.
-	if m := strings.Count(s, old); m == 0 {
-		if m = strings.Count(s, old2); m == 0 {
-			return s // avoid allocation
-		}
-		// return s // avoid allocation
-		// } else if n < 0 || m < n {
-	} else if true || m < n {
-		n = m
-	}
-
-	// Apply replacements to buffer.
-	var b strings.Builder
-
-	// in this use case  len(new) is always <= len(old)
-	// b.Grow(len(s) + n*(len(new)-len(old)))
-	b.Grow(len(s))
-
-	start := 0
-	for i := 0; i < n; i++ {
-		j := start
-		// in this use case, len(old) is always 1 or 2
-		// if len(old) == 0 {
-		// 	if i > 0 {
-		// 		_, wid := utf8.DecodeRuneInString(s[start:])
-		// 		j += wid
-		// 	}
-		// } else {
-		j += strings.Index(s[start:], old)
-		if j == -1 {
-			strings.Index(s[start:], old2)
-		}
-		// }
-		b.WriteString(s[start:j])
-		b.WriteString(new)
-		start = j + len(old)
-	}
-	b.WriteString(s[start:])
-	return b.String()
-}
-
-var global_sb = strings.Builder{}
-
-// reuse global string builder
-func normalizeGlobalStringsBuilder(s string) string {
-	global_sb.Reset()
-	global_sb.Grow(len(s))
-
-	var old int = 0
-
-	for {
-		if i := strings.Index(s[old:], "\r"); i > 0 {
-			global_sb.WriteString(s[old:i])
-
-			old = i + 1
-			if s[i+1] == '\n' {
-				old += 1
-			}
-		} else {
+	sb := strings.Builder{}
+	var start = 0
+	var current = start
+	for i := strings.Index(s[start:], "\r"); i > 0; {
+		sb.WriteString(s[start : start+current]) // write part without \r
+		start = i + 1                            // skip \r
+		if start > len(s) {                      // check for done
 			break
 		}
+		if s[i+1] != '\n' {
+			sb.WriteByte('\n') // add in nl to replace isolated \r
+		}
 	}
+	sb.WriteString(s[start:])
 
-	return global_sb.String()
+	return sb.String()
 }
 
 // normalizeNewlines normalizes \r\n (windows) and \r (mac)
@@ -121,37 +168,7 @@ func normalizeNewlines(d []byte) []byte {
 	return d
 }
 
-// normalizeNewlinesString uses strings.Replace to compare normalization efficiency
-func normalizeNewlinesString(d string) string {
-	// replace CR LF \r\n (windows) with LF \n (unix)
-	d = strings.Replace(d, "\r\n", "\n", -1)
-	// replace CF \r (mac) with LF \n (unix)
-	d = strings.Replace(d, "\r", "\n", -1)
-	return d
-}
-
+// for benchmarking comparisons
 func normalizeNewlinesBytesWrapper(d string) string {
 	return string(normalizeNewlines([]byte(d)))
-}
-
-func normalizeNLForLoop(s string) string {
-
-	old := []byte(s)
-	new := make([]byte, 0, len(old))
-
-	for i, c := range old {
-		switch c {
-		case '\r':
-			if old[i+1] == '\n' {
-				continue
-			}
-			new = append(new, '\n')
-		case '\n':
-			new = append(new, '\n')
-		default:
-			new = append(new, c)
-
-		}
-	}
-	return string(new)
 }
