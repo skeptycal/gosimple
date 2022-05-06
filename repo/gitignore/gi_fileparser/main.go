349d8781c5cea5d13dd3138547126fe498036a00
Michael Treanor <skeptycal@gmail.com>
2022-05-06 14:09:20 -0500

    update gitignore example

--- repo/gitignore/gi_fileparser/main.go
+++ repo/gitignore/gi_fileparser/main.go
@@ -41,18 +41,17 @@ type (
 
 func fileOpenOrCreate(
 	filename string,
-	container *file,
 	create bool,
-	extraBuffer float64) (io.Closer, error) {
-	_ = container
+	extraBuffer float64) (*file, io.Closer, error) {
+	f := file{}
 	fi, err := os.Stat(filename)
 	if err != nil {
 		if !errors.Is(err, os.ErrNotExist) {
-			return nil, err
+			return nil, nil, err
 		}
 		if !create {
 			// if file does not exist, create it based on bool parameter 'create'
-			return nil, err
+			return nil, nil, err
 		}
 	}
 
@@ -76,46 +75,45 @@ func fileOpenOrCreate(
 
 	name, err := filepath.Abs(fi.Name())
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
+	f.filename = name
 
-	f, err := os.OpenFile(name, os.O_RDONLY|os.O_CREATE, gofile.NormalMode)
+	fh, err := os.OpenFile(name, os.O_RDONLY|os.O_CREATE, gofile.NormalMode)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
+	f.isDirty = false
 
 	sizeFloat := float64(fi.Size())
 	size := int(sizeFloat * extraBuffer)
 	buf := bytes.NewBuffer(make([]byte, 0, size))
 
-	container = &file{
-		filename: name,
-		f:        f,
-		fi:       fi,
-		isDirty:  false,
-		buf:      buf,
-	}
+	f.f = fh
+	f.fi = fi
+	f.buf = buf
 
-	return f, nil
+	return &f, f.f, nil
 }
 
 // NewFile takes a pointer to a file struct and a filename.
 // It checks for existance of the file (or creates as necessary),
 // creates a new file structure in f, and returns an io.Closer
 // for use with defer in the calling code.
-func NewFile(filename string, f *file) io.Closer {
-	// var f = &file{}
-	closer, err := fileOpenOrCreate(filename, f, true, defaultBufferSizeMultiplier)
+func NewFile(filename string) (*file, io.Closer) {
+	f, closer, err := fileOpenOrCreate(filename, true, defaultBufferSizeMultiplier)
 	if err != nil {
-		return fakecloser.New()
+		return nil, fakecloser.NewFromError(err)
 	}
-	return closer
+	fmt.Println(f)
+	return f, closer
 }
 
 // LoadData loads all data from the source file into the
 // memfile in the *file struct.
 func (f *file) LoadData() error {
-	defer NewFile(f.fi.Name(), f)
+	f, closer := NewFile(f.fi.Name())
+	defer closer.Close()
 
 	n, err := io.Copy(f.buf, f.f)
 	if err != nil {
@@ -136,6 +134,10 @@ func (f *file) Bytes() []byte {
 	return f.buf.Bytes()
 }
 
+func (f *file) FileInfo() os.FileInfo {
+	return f.fi
+}
+
 func getFiCli(filename string, container *string) os.FileInfo {
 	fiIn, err := os.Stat(inFile)
 	if err != nil {
@@ -146,27 +148,28 @@ func getFiCli(filename string, container *string) os.FileInfo {
 
 // getDataCli gets the bytes from filename and puts a string
 // version in container.
-func getDataCli(filename string, container *string) error {
-	_ = container
+func getDataCli(filename string) (string, error) {
 	data, err := os.ReadFile(inFile)
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	s := b2s(data)
-	container = &s
-
-	return nil
+	return b2s(data), nil
 }
 
 func main() {
-	var f = file{}
-	defer NewFile(inFile, &f)
+	var f *file
+
+	f, closer := NewFile(inFile)
+	defer closer.Close()
+	// defer NewFile(inFile, f)
 
 	fmt.Println("gofile.PWD(): ", gofile.PWD())
+	fmt.Println("file name: ", f.FileInfo().Name())
+	fmt.Println("file size: ", f.FileInfo().Size())
 
-	fiIn := f.fi
-	fmt.Printf("Input File: %8v %15s %v\n", fiIn.Mode(), fiIn.Name(), fiIn.Size())
+	// fiIn := f.FileInfo()
+	// fmt.Printf("Input File: %8v %15s %v\n", fiIn.Mode(), fiIn.Name(), fiIn.Size())
 
 	w, err := os.OpenFile("../gitignore_gen.go", os.O_RDWR|os.O_CREATE, gofile.NormalMode)
 	if err != nil {
@@ -179,9 +182,9 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 
-	var data *string
+	// var data *string
 
-	err = getDataCli(inFile, data)
+	data, err := getDataCli(inFile)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -189,5 +192,6 @@ func main() {
 	_ = data
 
 	fmt.Println(data)
+	fmt.Println(f)
 
 }
